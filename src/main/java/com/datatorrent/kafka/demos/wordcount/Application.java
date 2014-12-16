de98eb1f126580f3829a3a8eaf41bc917f56900e
Siyuan Hua <siyuan@datatorrent.com>
2014-12-16 09:46:50 -0800

    fix null

--- src/main/java/com/datatorrent/kafka/demos/wordcount/Application.java
+++ src/main/java/com/datatorrent/kafka/demos/wordcount/Application.java
@@ -1,5 +1,7 @@
 package com.datatorrent.kafka.demos.wordcount;
 
+import java.util.HashSet;
+
 import org.apache.hadoop.conf.Configuration;
 
 import com.datatorrent.api.DAG;
@@ -7,7 +9,7 @@ import com.datatorrent.api.StreamingApplication;
 import com.datatorrent.api.Context.PortContext;
 import com.datatorrent.api.DAG.Locality;
 import com.datatorrent.api.annotation.ApplicationAnnotation;
-import com.datatorrent.contrib.kafka.KafkaSinglePortOutputOperator;
+import com.datatorrent.contrib.kafka.*;
 import com.datatorrent.kafkabenchmark.PartitionableKafkaInputOperator;
 import com.datatorrent.lib.algo.UniqueCounter;
 
@@ -19,7 +21,9 @@ public class Application implements StreamingApplication
  public void populateDAG(DAG dag, Configuration conf)
  {
    PartitionableKafkaInputOperator kafkainput = dag.addOperator("kafkawordinput", new PartitionableKafkaInputOperator());
+   KafkaConsumer consumer = new SimpleKafkaConsumer(null, 10000, 100000, "test_kafka_autop_client", new HashSet<Integer>());
    
+   kafkainput.setConsumer(consumer);
    UniqueCounter<String> wordCount = dag.addOperator("count", new UniqueCounter<String>());
    dag.addStream("kafkawordinput-count", kafkainput.oport, wordCount.data);
    
